fd283c75195d8cc8f01dcff9d080f6f39b0a15c4
egroehs <eduardagroehs@gmail.com>
2024-02-06 11:14:43 -0800

    clearBtn
    added clear button function

--- number.js
+++ number.js
@@ -1,19 +1,24 @@
 const input = document.getElementById("user-input");
 const check = document.getElementById("check-btn");
-const result = document.getElementById("results-text");
+const result = document.getElementById("results-div");
 const clear = document.getElementById("clear-btn");
 
-
 const regex = /^(1\s?-?)?((\(\d{3}\))|(\d{3}))\s?-?\d{3}-?\s?\)?\d{4}$/;
-  check.addEventListener("click", testPhone);
 
-function testPhone() {
-if(input.value === "") {
-    alert("Please provide a phone number")
-} else if (regex.test(input.value) === true) {
-    return result.innerText = `Valid US number: ${input.value}`
- } else {
-    return result.innerText = `Invalid US number: ${input.value}`
- }
+function testNumber() {
+  if (input.value === "") {
+    alert("Please provide a phone number");
+  } else if (regex.test(input.value) === true) {
+    return (result.innerText = `Valid US number: ${input.value}`);
+  } else {
+    return (result.innerHTML = `Invalid US number: ${input.value}`);
+  }
 }
 
+function clearFunc() {
+  result.innerText = "";
+}
+
+check.addEventListener("click", testNumber);
+
+clear.addEventListener("click", clearFunc);
